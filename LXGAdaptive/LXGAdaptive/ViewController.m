b62f17e64f1f98983a32884c1e9c2c1b49a06912
LXGiOS <ios_li@126.com>
2015-03-08 13:25:54 +0800

    Modify the comment

--- LXGAdaptive/LXGAdaptive/ViewController.m
+++ LXGAdaptive/LXGAdaptive/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional
     self.navigationController.navigationBar.backgroundColor = [UIColor orangeColor];
     self.view.backgroundColor = [UIColor whiteColor];
 
